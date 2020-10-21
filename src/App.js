64e1e539f9be185203b9fa9e10df6a20b4d92d1f
rishab1653 <rishab959290@gmail.com>
2020-10-21 13:51:34 +0530

    import modules in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react"
 import Navbar from './Navbar'
+import Jumbo from "./Jumbo"
+import AppSection from './AppSection'
+import Card from './Card'
+import Footer from './Footer'
+
+
+
 
 const App = () => {
     return(
         <div>
-            
             <Navbar/>
+            <Jumbo/>
+            <AppSection/>
+            <Card/>
+           <Footer/>
+            
         </div>
     )
 }
--- src/Card.js
+++ src/Card.js
@@ -9,7 +9,7 @@ const Card = () =>{
       </h2>
       <div className="row">
         <div className="col-4">
-          <div className="card" style="width: 18rem;">
+          <div className="card" style={{width: "18rem"}}>
             <img
               src="https://images.pexels.com/photos/3532557/pexels-photo-3532557.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
               className="card-img-top"
@@ -26,7 +26,7 @@ const Card = () =>{
           </div>
         </div>
         <div className="col-4">
-          <div className="card" style="width: 18rem;">
+          <div className="card"  style={{width: "18rem"}}>
             <img
               src="https://images.pexels.com/photos/3532544/pexels-photo-3532544.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
               className="card-img-top"
@@ -43,10 +43,10 @@ const Card = () =>{
           </div>
         </div>
         <div className="col-4">
-          <div className="card" style="width: 18rem;">
+          <div className="card" style={{width: "18rem"}}>
             <img
               src="https://images.pexels.com/photos/2522663/pexels-photo-2522663.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-              class="card-img-top"
+              className="card-img-top"
               alt="..."
             />
             <div className="card-body">
@@ -55,7 +55,7 @@ const Card = () =>{
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </p>
-              <a href="#" className="btn btn-success">Go somewhere</a>
+              <a href="#" className ="btn btn-success">Go somewhere</a>
             </div>
           </div>
         </div>
