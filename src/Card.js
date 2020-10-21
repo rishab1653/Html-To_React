0df98142ff54885ff1d2931c887fd58b683adb7c
rishab1653 <rishab959290@gmail.com>
2020-10-21 14:11:12 +0530

    Added card new and make your card just by giving titile button name and body

--- src/Card.js
+++ src/Card.js
@@ -1,4 +1,7 @@
 import React from "react"
+import CardNew from './CardNew'
+
+
 const Card = () =>{
     return (
          
@@ -9,21 +12,8 @@ const Card = () =>{
       </h2>
       <div className="row">
         <div className="col-4">
-          <div className="card" style={{width: "18rem"}}>
-            <img
-              src="https://images.pexels.com/photos/3532557/pexels-photo-3532557.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-              className="card-img-top"
-              alt="..."
-            />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" className="btn btn-success">Go somewhere</a>
-            </div>
-          </div>
+        <CardNew title="hello" myButton = "Click me" cardbody="Some quick example text to build on the card title and make up the bulk of the card's content."/>
+      
         </div>
         <div className="col-4">
           <div className="card"  style={{width: "18rem"}}>
